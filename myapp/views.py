ff04bda274264d410b14e2f7789b03389de03c57
aswin kondedan <aswin6804@gmail.com>
2021-12-03 16:11:07 +0530

    first commit

---
+++ myapp/views.py
@@ -0,0 +1,10 @@
+from django.http.response import HttpResponse
+from django.shortcuts import render
+
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def aboutfunction(request):
+    return render(request,'two.html')
