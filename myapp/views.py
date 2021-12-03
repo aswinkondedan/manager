2ee5088f2625918d12131c4bc5f6baf9435413bf
aswin kondedan <aswin6804@gmail.com>
2021-12-03 16:30:55 +0530

    sec

--- myapp/views.py
+++ myapp/views.py
@@ -6,5 +6,11 @@ from django.shortcuts import render
 # Create your views here.
 
 
+
+def homefunction(request):
+    return HttpResponse("hai")
+
+
+
 def aboutfunction(request):
     return render(request,'two.html')
